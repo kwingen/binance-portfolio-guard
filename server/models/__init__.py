dbf1438656e44f18ab668c3265b0cf65251e28bf
kwingen <kwingen@users.noreply.github.com>
2026-06-08 08:38:55 +0000

    fix: OrderRequest 导入缺失

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,9 +1,9 @@
 from server.models.auth import LoginRequest, LoginResponse, PasswordChangeRequest, SetupRequest
-from server.models.trading import PositionItem, AccountInfo, DashboardStatus, EmergencyCloseRequest
+from server.models.trading import PositionItem, AccountInfo, DashboardStatus, EmergencyCloseRequest, OrderRequest
 from server.models.settings import SettingsUpdate, SettingsInfo, MonitorControl
 
 __all__ = [
     "LoginRequest", "LoginResponse", "PasswordChangeRequest",
-    "PositionItem", "AccountInfo", "DashboardStatus", "EmergencyCloseRequest",
+    "PositionItem", "AccountInfo", "DashboardStatus", "EmergencyCloseRequest", "OrderRequest",
     "SettingsUpdate", "SettingsInfo", "MonitorControl",
 ]
